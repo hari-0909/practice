7edac9644bf2d19f84fc19c4f2dacedb34a462c8
hari <haribabuji2004@gmail.com>
2025-05-17 13:15:49 +0530

    added async-awaits.js

--- assignment3/promises.js
+++ assignment3/promises.js
@@ -1,39 +1,32 @@
 //1
 function downloadData(url) {
-  return new Promise((resolve, reject) => {
-    console.log(`Starting download from ${url}...`);
-    setTimeout(()=>{
-      const data=`Data from ${url}`;
-      console.log('Download complete.');
-      resolve(data);
-    },1000);
+  return new Promise((resolve) => {
+    console.log(`Downloading from ${url}...`);
+    setTimeout(() => {
+      resolve(`Data from ${url}`);
+    }, 1000);
   });
 }
 //2
-function writeToFile(data){
-  return new Promise((resolve,reject)=>{
-    console.log('Writing data to file...');
-    setTimeout(()=>{
-      const filename='output.txt';
-      console.log('File written:',filename);
-      resolve(filename);
-    },1000);
+function writeToFile(data) {
+  return new Promise((resolve) => {
+    console.log('Writing data...');
+    setTimeout(() => {
+      resolve('output.txt');
+    }, 1000);
   });
 }
 //3
-function uploadFile(filename){
-  return new Promise((resolve,reject)=>{
-    console.log(`Uploading ${filename} to server...`);
-    setTimeout(()=>{
-      const response =`${filename} uploaded successfully.`;
-      console.log('Upload complete.');
-      resolve(response);
-    },1000);
+function uploadFile(filename) {
+  return new Promise((resolve) => {
+    console.log(`Uploading ${filename}...`);
+    setTimeout(() => {
+      resolve(`${filename} uploaded`);
+    }, 1000);
   });
 }
 //4
 downloadData('https://example.com')
-  .then(data=>writeToFile(data))
-  .then(filename=>uploadFile(filename))
-  .then(response=>console.log('Final Result:',response))
-  .catch(error=>console.error('Error:',error));
+  .then(data => writeToFile(data))
+  .then(filename => uploadFile(filename))
+  .then(result => console.log('Result:', result));
